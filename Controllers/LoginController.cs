1c241869409689f23912fee859e85955dbed3b51
BealBC <benjaminbeal@Benjamins-MacBook-Pro.local>
2018-03-25 09:00:29 -0700

    Login MVC
    Adding Login MCV.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryProject.Models;
+
+
+namespace BakeryProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index([Bind(Include = "Email, Password")]LoginClass lc)
+        {
+            BakeryEntities db = new BakeryEntities();
+            int loginResult = db.usp_Login(lc.PersonEmail, lc.PersonPhone);
+            if (loginResult != -1)
+            {
+                var uid = (from r in db.People
+                           where r.PersonEmail.Equals(lc.PersonEmail)
+                           select r.PersonKey).FirstOrDefault();
+                int rKey = (int)uid;
+                Session["personKey"] = rKey;
+
+                MessageClass msg = new MessageClass();
+                msg.MessageText = "Thank You! You are now logged in as '" + lc.PersonEmail + "'.";
+                return RedirectToAction("Result", msg);
+            }
+            MessageClass message = new MessageClass();
+            message.MessageText = "Invalid Login";
+            return View("Result", message);
+        }
+        public ActionResult Result(MessageClass m)
+        {
+            return View(m);
+        }
+
+    }
+}
---
+++ Models/LoginClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BakeryProject.Models
+{
+    public class LoginClass
+    {
+        public LoginClass() { }
+
+        public LoginClass(string email, string password)
+        {
+            PersonEmail = email;
+            PersonPhone = password;
+        }
+
+        public string PersonEmail { set; get; }
+        public string PersonPhone { set; get; }
+    }
+}
